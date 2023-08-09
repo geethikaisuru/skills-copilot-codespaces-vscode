aa167915f2162ab8ed6893f041f4bb5b3b524e5f
geethikaisuru <35458073+geethikaisuru@users.noreply.github.com>
2023-08-09 22:41:04 +0530

    5th

---
+++ comments.js
@@ -0,0 +1,22 @@
+//Create web server and listen on port 3000
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Server listening at http://localhost:' + port);
+
+// Path: skills-copilot-codespaces-vscode\comments.js
+//Create web server and listen on port 3000
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Server listening at http://localhost:' + port);
+
+// Path: skills-copilot-codespaces-vscode\comments.js
+//Create web server and listen on port 3000
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Server listening at http://localhost:' + port);
